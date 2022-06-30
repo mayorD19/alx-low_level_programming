bb4f3a46167c9177382a94a21553377e793088b4
OJE DOMINION <dominionoje@gmail.com>
2022-06-30 11:18:17 -0700

    using the put file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,9 @@
-#include <stdio.h>
-
-  
-/**
- *
+#include <stdio.h> 
+/*
  * main - print the string in the put function
- *
- *
- *
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
- *
- *
+ * this program prints Programming is like building a multilingual puzzle
+ * Return0
  */
 int main(void)
 {
