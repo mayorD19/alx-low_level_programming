6844089b2766fcf7ee18556b372cc658bcc8b835
OJE DOMINION <dominionoje@gmail.com>
2022-06-30 15:18:16 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - print the sizes of different data type
  *
- * Description:using the main function 
+ * Description: using the main function 
  * this program prints "sizes of data types
  * Return: 0
  */
@@ -11,14 +11,14 @@ int main(void)
 {
 	int i;
 	char c;
-	long int li;
-	long long int lli;
+	long int l;
+	long long int k;
 	float f;
 
 	printf("The size of an char: %ld byte(s)\n", sizeof(c));
 	printf("The size of a int: %ld byte(s)\n", sizeof(i));
-	printf("The size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("The size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("The size of a long int: %ld byte(s)\n", sizeof(l));
+	printf("The size of a long long int: %ld byte(s)\n", sizeof(k));
 	printf("The size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
