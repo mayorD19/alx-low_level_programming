b1c9f09dc67059f4e2bfae1b41ecfd7f0404c077
OJE DOMINION <dominionoje@gmail.com>
2022-06-30 11:22:01 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h> 
-/*
+/**
  * main - print the string in the put function
  * Description: using the main function
  * this program prints Programming is like building a multilingual puzzle
