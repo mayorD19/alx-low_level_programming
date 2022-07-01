47d70f04d107145a1c8b857bf132d49e0c96897b
OJE DOMINION <dominionoje@gmail.com>
2022-06-30 17:25:00 -0700

    modified quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - A function that print out the last part of a quote in the standard error
- * returns: 1 if success.
+ * main - A function that print out a string 
+ *
+ * Description: using the write function to" print out a quote 
+ * Return: 1
  */ 
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 201510-19\n", 59);
 	return (1);
 }
