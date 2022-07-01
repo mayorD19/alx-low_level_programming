caa74cfa518eace02acdc826f4a28633ec324d6f
OJE DOMINION <dominionoje@gmail.com>
2022-06-30 17:32:34 -0700

    task 8 being modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - A function that print out a string 
  *
- * Description: using the write function to" print out a quote 
- * Return: 1
+ * Description: using the write function to "print out a quote 
+ * Return:1
  */ 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 201510-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
