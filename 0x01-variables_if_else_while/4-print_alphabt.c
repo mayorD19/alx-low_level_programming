f9b56de412d4312ab35dea3f2c28dffe1f8761d3
OJE DOMINION <dominionoje@gmail.com>
2022-07-01 15:21:40 -0700

    Ommiting q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints lowercase alphabets
+ *
+ * Description using the lowercase without q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return(0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@
 1-Printing Last digit
 2-Printing A-A letters
 3-Printing Uppercase and Lower case
+4-Printing in lowercase without q and e
