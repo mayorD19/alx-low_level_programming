e93a7317f5bf75585bd6500ed3eb78fe05424e35
OJE DOMINION <dominionoje@gmail.com>
2022-06-30 17:48:26 -0700

    modification 5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
 /**
  * main - A function that print out a string
  *
- * Description: using the write function to "printing out a quote, 
- * Return:1
- */ 
+ * Description: using the write function to "printing out a quote,
+ *Return:1
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
