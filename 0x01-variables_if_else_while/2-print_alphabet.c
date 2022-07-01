a007e213a08b93d897cf2ae5c63c7e5603f1ff40
OJE DOMINION <dominionoje@gmail.com>
2022-07-01 12:26:32 -0700

    jsfdg9i

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - A function to print alphabets
+ *
+ * Description using the putchar function we print the the alphabets in small case
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0-Printing a random number
 1-Printing Last digit
+2-Printing A-A letters
