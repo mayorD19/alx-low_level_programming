2372fb0a0798f4f14d66ec93bb84641181e3d820
OJE DOMINION <dominionoje@gmail.com>
2022-07-05 18:27:22 -0700

    check code

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,12 +10,8 @@ int main(void)
 	int i, result;
 
 	for (i = 0; i < 1024; i++)
-	{
 		if (i % 3 == 0 || i % 5 == 0)
-		{
-			result += 1;
-		}
+			result += i;
 	printf("%d\n", result);
 	return (0);
-	}
 }
