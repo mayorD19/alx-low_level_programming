12d72a2c177314a4323ae4ca6f74f6ad64e178cb
OJE DOMINION <dominionoje@gmail.com>
2022-07-07 09:30:53 -0700

    Looping through numbvers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - to print from zero to nine
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int c;
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0-ckecking for uppercase charcaters
 1-Checking for the digit 0
 2-Multiplying two numbers
+3-Printing 0-9
