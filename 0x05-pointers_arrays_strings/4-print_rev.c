e6d58f9c3956608d365d3923016a7430eac0a408
OJE DOMINION <dominionoje@gmail.com>
2022-07-12 13:31:31 -0700

    reversing string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - A function that prints in reverse a string
+ *
+ * @s: The character inputed
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 1-Swapping Horses a Taboo!
 2-Finding the length of strings
 3-function that prints a string followed by a new line to stdout
+4-A function that prints a string in the reverse order
