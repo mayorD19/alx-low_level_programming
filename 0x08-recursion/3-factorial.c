3de393147fab4e0d2807ff884ede473cb3fee7c0
OJE DOMINION <dominionoje@gmail.com>
2022-07-20 09:57:32 -0700

    Find the Factorial of a Number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - A function that prints the factorial of numbers
+ *
+ * @n: The charcter passed in as arguments
+ * Return: s
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	return n * factorial(n - 1);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,4 @@
 0- A function that prints out a string
 1-A function that prints a sting in reverse
 2-A function that returns the legnth of a string
+3-A function to return the factorial of a given number
