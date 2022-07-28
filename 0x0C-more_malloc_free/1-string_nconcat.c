252abecb73b0b6c6e853122db25d3ca9460d6480
OJE DOMINION <dominionoje@gmail.com>
2022-07-28 08:10:30 -0700

    concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,32 +1,52 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-/**
- * string_nconcat - concatenates two strings
- * @s1:stirng 1
- * @s2:string 2
- * @n: n bytesofstring 2
- * Return: pointer
- */
+#include <stdlib.h>
 
+/**
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr, *empt;
-	unsigned int i, k;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-	empt = "";
 	if (s1 == NULL)
-		s1 = empt;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = empt;
-	for (i = 0; s1[i] != '\0'; i++)
-		ptr = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
-	if (ptr == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		ptr[i] = s1[i];
-	for (k = 0; s2[k] != '\0' && k < n; k++, i++)
-		ptr[i] = s2[k];
-	ptr[i] = '\0';
-	return (ptr);
+
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
