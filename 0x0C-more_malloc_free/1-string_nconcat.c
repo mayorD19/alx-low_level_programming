ec52f48be592eb82300195ccac312abbdb48fa01
OJE DOMINION <dominionoje@gmail.com>
2022-07-28 07:50:05 -0700

    Concatenate string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,33 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * string_nconcat - concatenate two strings up to n bytes.
- * @s1: source string
- * @s2: string to truncate up to n bytes
- * @n: number of bytes to truncate by
- * Return: pointer to new buffer
+ * string_nconcat - concatenates two strings
+ * @s1:stirng 1
+ * @s2:string 2
+ * @n: n bytesofstring 2
+ * Return: pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int s1count, s2count, sizeBuffer, i;
+  char *ptr, *empt;
+  unsigned int i, k;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (s1count = 0; s1[s1count]; s1count++)
-		;
-	for (s2count = 0; s2[s2count]; s2count++)
-		;
-	s2count > n ? (s2count = n) : (n = s2count);
-	sizeBuffer = s1count + s2count + 1;
-	p = malloc(sizeBuffer * sizeof(char));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < sizeBuffer - 1; i++)
-		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
-	p[sizeBuffer] = '\0';
-	return (p);
+  empt = "";
+  if (s1 == NULL)
+    s1 = empt;
+  if (s2 == NULL)
+    s2 = empt;
+  for (i = 0; s1[i] != '\0'; i++)
+    ;
+  ptr = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
+  if (ptr == NULL)
+    return (NULL);
+  for (i = 0; s1[i] != '\0'; i++)
+    ptr[i] = s1[i];
+  for (k = 0; s2[k] != '\0' && k < n; k++, i++)
+    ptr[i] = s2[k];
+  ptr[i] = '\0';
+  return (ptr);
 }
