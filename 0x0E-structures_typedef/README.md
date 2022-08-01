981478f4a5f33a15bfcf53828a662225c2e95797
OJE DOMINION <dominionoje@gmail.com>
2022-08-01 14:40:31 -0700

    Define a new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,4 @@
 0-A function that creates a structure for a Dog
 1-a function that initialize a variable of type struct dog
 2-A function that prints a struct dog
+3-A function  Define a new type dog_t as a new name for the type struct dog.
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,12 +19,12 @@ typedef struct dog
 	float age;
 	char *owner;
 }
-dog_d;
+dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_d *new_dog(char *name, float age, char *owner);
-void free_dog(dog_d *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
