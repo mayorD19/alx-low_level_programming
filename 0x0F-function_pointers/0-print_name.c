e8a5d73115efe61526b550bddef512c2045ccb85
OJE DOMINION <dominionoje@gmail.com>
2022-08-03 16:29:43 -0700

    Create Function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - A function that prints name
+ * 
+ * @name: A name parameter
+ * @f: A pointer to a function
+ * Return: Always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
